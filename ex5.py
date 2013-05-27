2a34bf554ca15bf9524a3dd1ffe30e618dc59006
krvc <krvc.it@gmail.com>
2013-05-27 12:03:25 +0530

    Added Ex5

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'KRVC'
+my_age = 23 # not a lie
+my_height = 68.4 # inches
+my_weight = 65 # kgs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d kgs heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
+
